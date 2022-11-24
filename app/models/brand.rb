77a3678b6a1c8fc7a5bcb68ad7fa7e9319b76455
adrien-gllmt <adri.glmt@gmail.com>
2022-11-24 19:42:53 +0100

    marche po

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,9 +1,6 @@
 class Brand < ApplicationRecord
   belongs_to :supplier
-<<<<<<< HEAD
   has_many :products
-=======
->>>>>>> main/master
 
   scope :get_brand_id, -> (brand) { where('brand_name LIKE ?', brand).ids[0] }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_03_193340) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_24_175543) do
   create_table "brands", charset: "utf8", force: :cascade do |t|
     t.string "brand_name"
     t.bigint "supplier_id"
@@ -70,6 +70,18 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_03_193340) do
   create_table "suppliers", charset: "utf8", force: :cascade do |t|
     t.string "supp_name"
     t.float "supp_coeff"
+  end
+
+  create_table "users", charset: "utf8", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
